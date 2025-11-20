b983c40a8d788f0b101ef754a766d585e7a9df88
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-20 23:27:37 +0000

    added a TypeScript program that provides 4 lines of text to input whatever the user wants.

---
+++ main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-11-20
+ * @fileoverview this program provides 4 lines of text to input whatever the user wants.
+ */
+
+String(prompt("Can you give me four lines of text?"));
+String(prompt("Line1: "));
+String(prompt("Line2: "));
+String(prompt("Line3: "));
+String(prompt("Line4: "));
+
+console.log("\nDone");
