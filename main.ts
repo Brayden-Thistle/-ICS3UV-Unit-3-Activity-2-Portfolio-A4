ae358aa95e8b12c2ce4d4be654e401597fd6d713
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-21 22:35:54 +0000

    finished the rest of the code

--- main.ts
+++ main.ts
@@ -5,10 +5,17 @@
  * @fileoverview this program provides 4 lines of text to input whatever the user wants.
  */
 
+let line1: string;
+let line2: string;
+let line3: string;
+let line4: string;
+
 String(prompt("Can you give me four lines of text?"));
-String(prompt("Line1: "));
-String(prompt("Line2: "));
-String(prompt("Line3: "));
-String(prompt("Line4: "));
+line1 = String(prompt("Line1: "));
+line2 = String(prompt("Line2: "));
+line3 = String(prompt("Line3: "));
+line4 = String(prompt("Line4: "));
+
+console.log(line4 + line3 + line2 + line1)
 
 console.log("\nDone");
